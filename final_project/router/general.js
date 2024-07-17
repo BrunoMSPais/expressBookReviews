26195460193cc49c5583d38c1c676d313d49380b
BrunoMSPais <bpais@outlook.pt>
2024-07-17 17:36:10 -0400

    task 10

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,13 +27,27 @@ public_users.post("/register", (req,res) => {
   return res.status(200).send(JSON.stringify({ message: `The user ${username} was added to the users list.` }, null, 4));
 });
 
+let getBooks = () =>{
+    return new Promise((resolve,reject) => {
+        setTimeout(
+          resolve({
+            message: "Operation was successfull",
+            data: books
+          }), 250);
+    })
+}
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  if (!books) {
+  const booksAPICall = await getBooks();
+
+  const bookList = booksAPICall.data;
+
+  if (!bookList) {
     return res.status(404).send(JSON.stringify({ message: 'No books found' }, null, 4));
   } else {
-    return res.status(200).send(JSON.stringify({ books }, null, 4));
+    return res.status(200).send(JSON.stringify(bookList, null, 4));
   }
 });
 
@@ -47,7 +61,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   if (!book) return res.status(404).send(JSON.stringify({ message: 'Book not found' }, null, 4));
 
   return res.status(200).send(JSON.stringify({ book }, null, 4));
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
